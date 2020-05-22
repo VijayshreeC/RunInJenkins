f3bd43027f0cc8d19f0279136e098bfa26c367fb
VijayshreeC <vijayshree.chaturvedi@citiustech.com>
2020-05-22 16:29:21 +0530

    Added

--- src/test/java/com/Testcases/BrowserClass.java
+++ src/test/java/com/Testcases/BrowserClass.java
@@ -24,6 +24,10 @@ public class BrowserClass {
 
 	String url = "https://www.google.com/";
 	
+	public static final String USERNAME = "Admin_00";
+	  public static final String ACCESS_KEY = "b94209bf-d332-40b2-9487-66354d6d2de9";
+	  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.us-west-1.saucelabs.com:443/wd/hub";
+	
 	@Parameters("browser")
 	
 	@BeforeClass
@@ -39,6 +43,7 @@ public class BrowserClass {
 			
 		} 
 		else if (browsername.equals("firefox")) {
+			System.out.println("Running in firefox");
 			driver = new FirefoxDriver();
 			driver.manage().window().maximize();
 		} 
@@ -49,11 +54,24 @@ public class BrowserClass {
 			DesiredCapabilities dc = new DesiredCapabilities();
 			dc.setBrowserName("chrome");
 			dc.setPlatform(Platform.WINDOWS);
-			driver = new RemoteWebDriver(new URL("http://192.168.0.104:4430/wd/hub"), dc);
+			driver = new RemoteWebDriver(new URL("http://192.168.0.104:4444/wd/hub"), dc);
 			System.out.println("RUNNING TESTS IN REMOTE CHROME BROWSER");
 			System.out.println();
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		}
+		else if(browsername.equals("saucechrome")) {
+			System.out.println("Running in Saucelabs");
+			System.out.println(URL);
+			DesiredCapabilities caps = DesiredCapabilities.chrome();
+		    caps.setCapability("platform", "Windows 10");
+		    caps.setCapability("version", "latest");
+		    caps.setCapability("name", "Test1");
+		    caps.setCapability("buildNumber", "3.0");
+		 
+		    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+		    driver.get(url);
+			driver.manage().window().maximize();
+}
 		
 	}
 
